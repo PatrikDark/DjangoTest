4c9f633322c987670b3a0ec724d5dafd72f455a8
Suman <desuman18@gmail.com>
2020-07-18 22:25:33 +0530

    Static files for productions

--- Task/settings.py
+++ Task/settings.py
@@ -110,4 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# for production only ...
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# run python manage.py collectstatic
 LOGIN_URL = 'login'
+
